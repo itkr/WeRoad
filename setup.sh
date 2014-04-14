f4086eb6025c9d8aed512027554d3fe38328c708
itkr <g600392@gmail.com>
2014-04-15 02:11:03 +0900

    Update setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,10 @@
+#sudo apt-get install python-dev
+#curl -O http://python-distribute.org/distribute_setup.py
+#sudo python distribute_setup.py
+#curl -O https://raw.github.com/pypa/pip/master/contrib/get-pip.py
+#sudo python get-pip.py
+#sudo pip install virtualenv
+
 pip install Fabric==1.8.3
 pip install python-twitter==1.3.1
 pip install bpython==0.12
