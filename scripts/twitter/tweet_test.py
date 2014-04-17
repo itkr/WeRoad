8aca34365408f694715f40261b42d415470d4330
itkr <g600392@gmail.com>
2014-04-17 21:48:58 +0900

    Add dummy tweet script

---
+++ scripts/twitter/tweet_test.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import codecs
+import random
+import string
+import hashlib
+import twitter
+import settings
+import datetime
+
+
+def get_letters():  
+    source = string.digits + string.letters
+    letters = u"".join([random.choice(source) for i in xrange(25)])
+    letters_hash = hashlib.sha256(letters).hexdigest()
+    return letters_hash
+
+
+def tweet(message):
+    twitter_api = twitter.Api(
+        consumer_key=settings.twitter.API_KEY,
+        consumer_secret=settings.twitter.API_SECRET,
+        access_token_key=settings.twitter.ACCESS_TOKEN_KEY,
+        access_token_secret=settings.twitter.ACCESS_TOKEN_SECRET)
+    twitter_api.PostUpdates(message)
+
+
+def main():
+    message = u" ".join([
+        u"[自動テスト]", 
+        u"【date】:",
+        unicode(datetime.datetime.now()),
+        u"【code】:",
+        get_letters(),
+    ])
+    tweet(message)
+    print message
+
+
+if __name__ == '__main__':
+    sys.stdout = codecs.getwriter('utf8')(sys.stdout)
+    main()
+
