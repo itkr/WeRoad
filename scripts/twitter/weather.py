acef5816aeb2a0179f03cd9b0c3ee625abfe12bc
itkr <g600392@gmail.com>
2014-05-06 02:12:51 +0900

    Update weekday message

--- scripts/twitter/weather.py
+++ scripts/twitter/weather.py
@@ -19,6 +19,20 @@ class Controller(object):
         # TODO: dataを隠蔽
         self.wheather = Weather(location_id).get_data()
 
+    def get_weekday_name(self, date):
+        return [u"月", u"火", u"水", u"木", u"金", u"土", u"日"][date.weekday()]
+
+    def get_weekday_message(self, date):
+        message = [
+            u"今週もぼんぼりましょう_(´ཀ`」 ∠)_",
+            u"深夜アニメまで頑張ろう_(:3 ⌒ﾞ)_",
+            u"働きたくないでござる(´；ω；｀)",
+            u"中途半端な曜日やね！",
+            u"もうすぐ週末＼＼\\ ٩( 'ω' )و //／／",
+            u"今日は何しようか(´⊙ω⊙｀)",
+            u"｜ω・）月曜日の影", ]
+        return message[date.weekday()]
+
     # TODO: ストラテジパターンとか使って分ける
     def make_message(self):
         prefecture = self.wheather.get("location").get("prefecture")  # 都道府県
@@ -26,6 +40,7 @@ class Controller(object):
             if forecasts.get("dateLabel") == u"今日":
                 break
         today = datetime.datetime.strptime(forecasts.get("date"), '%Y-%m-%d')
+        weekday = self.get_weekday_name(today)
         public_time = self.wheather.get("publicTime")
 
         max_temperature = min_temperature = None
@@ -35,25 +50,27 @@ class Controller(object):
             min_temperature = forecasts.get("temperature").get("min").get('celsius')
 
         if max_temperature and min_temperature:
-            message_format = u"[自動] おはよう。 {month}月{day}日 {prefecture}は{telop} 気温{min_temperature}〜{max_temperature}度"
+            message_format = u"[自動] おはよう。 {month}月{day}日({weekday}) {prefecture}は{telop} 気温{min_temperature}〜{max_temperature}度 {extend_message}"
         elif max_temperature:
-            message_format = u"[自動] おはよう。 {month}月{day}日 {prefecture}は{telop} 最高気温は{max_temperature}度です"
+            message_format = u"[自動] おはよう。 {month}月{day}日({weekday}) {prefecture}は{telop} 最高気温は{max_temperature}度です {extend_message}"
         elif min_temperature:
-            message_format = u"[自動] おはよう。 {month}月{day}日 {prefecture}は{telop} 最低気温は{min_temperature}度です"
+            message_format = u"[自動] おはよう。 {month}月{day}日({weekday}) {prefecture}は{telop} 最低気温は{min_temperature}度です {extend_message}"
         else:
-            message_format = u"[自動] おはよう。 {month}月{day}日 {prefecture}の天気は{telop}みたいです。"
+            message_format = u"[自動] おはよう。 {month}月{day}日({weekday}) {prefecture}の天気は{telop}みたいです。 {extend_message}"
 
         return message_format.format(
             month=today.month,
             day=today.day,
+            weekday=weekday,
             prefecture=prefecture,
             telop=forecasts.get("telop"),  # "晴れ"など
             max_temperature=max_temperature,  # 最高気温
-            min_temperature=min_temperature)  # 最低気温
+            min_temperature=min_temperature,  # 最低気温
+            extend_message=self.get_weekday_message(today))
 
     def send(self):
         message = self.make_message()
-        self.twitter_api.PostUpdates(message)
+#        self.twitter_api.PostUpdates(message)
         return message
 
 
@@ -62,7 +79,7 @@ def main(argv):
     print "location_id:", location_id
     c = Controller(location_id)
     message = c.send()
-    print message
+    print "".join([message, u" 【", str(len(message)), u"文字】"])
 
 
 if __name__ == '__main__':
